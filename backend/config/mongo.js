295a0fb1f24d0e82c5981a37231a3e4801165bf6
Amin <hamadagood111@gmail.com>
2019-10-16 01:09:21 +0200

    Added cors package

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -2,7 +2,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 }, (error) => {
     if (error) {
         console.error(`Mongoose connection failed with the following error ${error}`);
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
+const {mongo} = require('./config');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const {mongo} = require('./config');
 
 app.use(express.urlencoded({
     extended: true
 }));
 
+app.use(cors());
 app.use(express.json());
 
 const router = require(path.join(__dirname, 'routes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Express server listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Express server listening on: ${PORT}`));
