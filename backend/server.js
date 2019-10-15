b634dd94cc7f8740af356f393059126aa6cf0369
Amin <hamadagood111@gmail.com>
2019-10-15 21:14:36 +0200

    No need to body parser on the new express

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,13 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const {mongo} = require('./config');
 
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
     extended: true
 }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 const router = require(path.join(__dirname, 'routes'));
 
