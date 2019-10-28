35ed509b8a31593e5402e8b8f56ca949b5aadfe1
Amin <hamadagood111@gmail.com>
2019-10-28 16:57:17 +0100

    Create a new user on the front-end done

--- frontend/src/components/SignUpForm/index.js
+++ frontend/src/components/SignUpForm/index.js
@@ -1,74 +1,160 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } from 'mdbreact';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
+import { Link } from "react-router-dom";
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-const SignUpForm = () => {
-  return (
-    <MDBContainer>
+    this.onChangeFirstName = this.onChangeFirstName.bind(this);
+    this.onChangeLastName = this.onChangeLastName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangeMobile = this.onChangeMobile.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobile: '',
+      password: ''
+    }
+  }
+
+  onChangeFirstName(e) {
+    this.setState({
+      firstName: e.target.value,
+    })
+  };
+
+  onChangeLastName(e) {
+    this.setState({
+      lastName: e.target.value,
+    })
+  };
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value,
+    })
+  };
+
+  onChangeMobile(e) {
+    this.setState({
+      mobile: e.target.value,
+    })
+  };
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value,
+    })
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+    const user = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      mobile: this.state.mobile,
+      password: this.state.password
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/register', user)
+      .then(res => console.log(res.data))
+      .catch(error => {
+        console.log(error.response);
+      });
+
+    this.setState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      mobile: '',
+      password: '',
+    })
+  }
+
+  render() {
+    return (
+      <MDBContainer>
       <MDBRow>
         <MDBCol md="6">
-          <MDBCard>
-            <div className="header pt-3 grey lighten-2">
-              <MDBRow className="d-flex justify-content-start">
-                <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5">
-                  Skapa ett konto
-                </h3>
-              </MDBRow>
+          <form onSubmit={this.onSubmit}>
+            <p className="h4 text-center mb-4">Sign up</p>
+            <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
+              First name
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              value={this.state.firstName}
+              onChange={this.onChangeFirstName}
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
+              Last name
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              value={this.state.lastName}
+              onChange={this.onChangeLastName}
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
+              email
+            </label>
+            <input
+              type="email"
+              id="defaultFormRegisterEmailEx"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+            <br />
+            <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
+              mobile
+            </label>
+            <input
+              type="text"
+              id="defaultFormRegisterNameEx"
+              className="form-control"
+              value={this.state.mobile}
+              onChange={this.onChangeMobile}
+            />
+            <br />
+            <label
+              htmlFor="defaultFormRegisterPasswordEx"
+              className="grey-text"
+            >
+              Your password
+            </label>
+            <input
+              autoComplete=""
+              type="password"
+              id="defaultFormRegisterPasswordEx"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+            <div className="text-center mt-4">
+              <MDBBtn color="unique" type="submit">
+                Register
+              </MDBBtn>
             </div>
-            <MDBCardBody className="mx-4 mt-4">
-              <MDBInput label="E-post" group type="text" validate />
-              <MDBInput
-                label="förenamn"
-                group
-                type="text"
-                validate
-                containerClass="mb-0"
-              />
-
-              <MDBInput
-                label="efternamn"
-                group
-                type="text"
-                validate
-                containerClass="mb-0"
-              />
-
-              <MDBInput
-                label="Mobile nummer"
-                group
-                type="text"
-                validate
-                containerClass="mb-0"
-              />
-
-              <MDBInput
-                label="Lösenors"
-                group
-                type="password"
-                validate
-                containerClass="mb-0"
-              />
-              <div className="text-center mb-4 mt-5">
-                <MDBBtn
-                  color="danger"
-                  type="button"
-                  className="btn-block z-depth-2"
-                >
-                  Skapa ett konto
-                </MDBBtn>
-              </div>
-              <p className="font-small grey-text d-flex justify-content-center">
-                Är du redan kund?
-                <Link to="/login" className="dark-grey-text font-weight-bold ml-1">
-                  Logga in
-                </Link>
-              </p>
-            </MDBCardBody>
-          </MDBCard>
+          </form>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
-  );
+    );
+  }
 };
 
 export default SignUpForm;
