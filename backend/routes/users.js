18ddbcf56798dafaa7e7330aa17cdc782c19bfa1
Amin <hamadagood111@gmail.com>
2019-10-30 14:52:02 +0100

    Added get user by token route to the user api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
 const userController = require('../controllers/users/users');
 const registerController = require('../controllers/users/register');
 const authenticationController = require('../controllers/users/authentication');
+const auth = require('../middleware/authentication');
+const User = require('../models/User');
 
 router.route('/')
   .get(registerController.getAllUsersAndCars);
@@ -22,5 +23,14 @@ router.route('/:userId/cars')
   .post(userController.addCarForSale)
   .get(userController.getSellerCars);
   // ADD DeleteByUserId AND UpdateByUserId METHODS??
+
+// @route   GET users/auth/user
+// @desc    Get user data
+// @access  Private
+router.get('/auth/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+});
 
 module.exports = router;
--- frontend/src/pages/Index/index.js
+++ frontend/src/pages/Index/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import DefaultLayout from '../../layouts/index';
+import axios from 'axios';
+class index extends React.Component {
+  constructor(props) {
+    super(props);
 
-const Index = () => {
+    this.state = {
+      username: null,
+      loggedIn: false
+     };
+  }
 
-  return(
-  <DefaultLayout>
-     {/* INDEX PAGE CONTENT */}
+  getUser() {
+      axios.get('/user/').then(response => {
+        console.log('Get user response: ')
+        console.log(response.data)
+        if (response.data.user) {
+          console.log(
+            'Get User: There is a user saved in the server session: ')
 
-    Index Page
-  </DefaultLayout >
-  );
+          this.setState({
+            loggedIn: true,
+            username: response.data.user.username
+          })
+        } else {
+          console.log('Get user: no user');
+          this.setState({
+            loggedIn: false,
+            username: null
+          })
+        }
+      })
+    }
 
+  render() {
+    return (
+      <DefaultLayout>
+          {/* INDEX PAGE CONTENT */}
+
+        Index Page
+      </DefaultLayout >
+    );
+  }
 }
 
-export default Index;
+export default index;
