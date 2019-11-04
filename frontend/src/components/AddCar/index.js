356ddd4d4d3f99a8bffe4e8781c21bd254bef667
Amin <hamadagood111@gmail.com>
2019-11-04 16:01:51 +0100

    Fixed add car from

--- frontend/src/components/AddCar/index.js
+++ frontend/src/components/AddCar/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import axios from 'axios';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn} from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput} from 'mdbreact';
 import { Link, Redirect } from "react-router-dom";
-
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+import './AddCar.css'
 class AddCar extends React.Component {
   constructor(props) {
     super(props);
@@ -37,87 +39,84 @@ class AddCar extends React.Component {
   // }
 
   render() {
+    const options = [
+      'one', 'two', 'three'
+    ]
+    const defaultOption = options[0];
     return (
       <MDBContainer>
         <MDBRow>
           <MDBCol md="6">
             <div className="sign-in">
                 <h3 className="my-3">
-                  <strong className="font-weight-bold">Lägg in din bil annons</strong>
+                  <strong className="font-weight-bold"> Lägg in din bil annons</strong>
                 </h3>
               <br />
             </div>
             <form onSubmit={this.onSubmit}>
-              <label htmlFor="firstName" className="dark-grey-text">
-                Title
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                className="form-control"
-                value={this.state.firstName}
-                onChange={this.handleChange}
-                name="firstName"
+              <p className="text-left">Bilder </p>
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupFileAddon01">Ladda upp </span>
+                </div>
+                <div className="custom-file">
+                  <input type="file" id="inputGroupFile01" ria-describedby="inputGroupFileAddon01"/>
+                  <label className="custom-file-label" htmlFor="inputGroupFile01">
+                    Välj bild
+                  </label>
+                </div>
+              </div>
+              <br />
+              <br />
+              <p className="text-left">Typ av bil </p>
+              <Dropdown options={options} onChange={this._onSelect} value={defaultOption}
+                placeholder="Select an option"
               />
               <br />
-              <label htmlFor="lastName" className="dark-grey-text">
-                Beskrivning
-              </label>
-              <input
-                type="text"
-                id="lastName"
-                className="form-control"
+              <p className="text-left">Märke </p>
+              <Dropdown options={options} onChange={this._onSelect} value={defaultOption}
+                placeholder="Select an option"
+              />
+              <br />
+              <p className="text-left">Modell </p>
+              <Dropdown options={options} onChange={this._onSelect}
+                value={defaultOption}
+                placeholder="Select an option"
+              />
+              <br />
+              <p className="text-left">Modellår </p>
+              <Dropdown options={options} onChange={this._onSelect} value={defaultOption}
+                placeholder="Select an option"
+              />
+              <br />
+              <p className="text-left">Bränsle </p>
+              <Dropdown options={options} onChange={this._onSelect} value={defaultOption}
+                placeholder="Select an option"
+              />
+              <br />
+              <p className="text-left">Växellåda </p>
+              <Dropdown options={options} onChange={this._onSelect} value={defaultOption}
+                placeholder="Select an option"
+              />
+              <br />
+              <p className="text-left"> Anonns titel </p>
+              <input type="text" id="titel" className="form-control" name="titel" value={this.state.titel}
+                onChange={this.handleChange} />
+              <br />
+              <p className="text-left">Pris </p>
+              <input type="text" id="adDescription" className="form-control" name="adDescription"
                 value={this.state.lastName}
-                onChange={this.handleChange}
-                name="lastName"
-              />
+                onChange={this.handleChange} />
               <br />
-              <label htmlFor="email" className="dark-grey-text">
-                Categoeory
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="form-control"
-                value={this.state.email}
-                onChange={this.handleChange}
-                name="email"
-              />
+              <MDBInput type="textarea" label="Beskrivning" rows="5" />
               <br />
-              <label htmlFor="mobile" className="dark-grey-text">
-                Märke
-              </label>
-              <input
-                type="number"
-                id="mobile"
-                className="form-control"
-                value={this.state.mobile}
-                onChange={this.handleChange}
-                name="mobile"
-              />
-              <br />
-              <label
-                htmlFor="password"
-                className="dark-grey-text"
-              >
-                Modell
-              </label>
-              <input
-                autoComplete="password"
-                type="password"
-                id="password"
-                className="form-control"
-                value={this.state.password}
-                onChange={this.handleChange}
-                name="password"
-              />
               <div className="text-center mt-4">
                 <MDBBtn color="btn btn-success" type="submit" className="font-weight-bold">
                   Lägg in annons
                 </MDBBtn>
               </div>
-              <br />
             </form>
+            <br />
           </MDBCol>
         </MDBRow>
       </MDBContainer>
