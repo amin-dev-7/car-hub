6de944c7795f8d0aa5fe9c7ca3710637f7f61683
Amin <hamadagood111@gmail.com>
2019-11-04 18:20:32 +0100

    Small fixes

--- frontend/src/components/AddCar/index.js
+++ frontend/src/components/AddCar/index.js
@@ -29,31 +29,7 @@ class AddCar extends React.Component {
       ],
 
     }
-    // const carOptions = [
-    //   'Sedan', 'SUV', 'Småbil', 'Kombi', 'Halvkombi', 'Coupé'
-    // ]
-
-    // const carYear = [
-    //   '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006',
-    //   '2007',
-    //   '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015',
-    //   '2016',
-    //   '2017', '2018', '2019'
-    // ]
-
-    // const fuelOptions = [
-    //   'El', 'Bensin', 'Diesel', 'Hybrid'
-    // ]
-
-    // const gearboxOptions = [
-    //   'Automat', 'Manuell'
-    // ]
-
-    // const defaultCarOption = carOptions[0];
-    // const defaultYearOption = carYear[20];
-    // const defaultFuelOpstion = fuelOptions[0];
-    // const defaultGearboxOption = gearboxOptions[0];
-    }
+  }
 
   // handleChange(e) {
   //   this.setState({
