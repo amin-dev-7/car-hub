4b67518b3963ee4077058ad806ed38a1e5d132cb
Amin <hamadagood111@gmail.com>
2019-11-12 20:53:31 +0100

    Started with delete car by user id

--- frontend/src/components/Car/CarCard/index.js
+++ frontend/src/components/Car/CarCard/index.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle,MDBCardText, MDBCol, MDBListGroupItem }
 from 'mdbreact';
+import axios from 'axios';
 
-const CarCard = (props) => {
-  return (
-    <MDBCol>
-      <MDBCard style={{ width: "22rem" }}>
-        {/* <MDBCardImage className="img-fluid" src={this.car.carImage} waves /> */}
-        <MDBCardBody>
-          <MDBCardTitle>{props.carCard.adTitle}</MDBCardTitle>
-          <MDBCardText>
-            {props.carCard.adDescription}
-          </MDBCardText>
-            <MDBListGroupItem>Pris: {props.carCard.price} SEK</MDBListGroupItem>
-            <MDBListGroupItem>Typ av bil: {props.carCard.carCategory}</MDBListGroupItem>
-            <MDBListGroupItem>Märke: {props.carCard.carBrand}</MDBListGroupItem>
-            <MDBListGroupItem>Modellår: {props.carCard.carModelYear}</MDBListGroupItem>
-            <MDBListGroupItem>Drivmedel: {props.carCard.carFuel}</MDBListGroupItem>
-            <MDBListGroupItem>Växellåda: {props.carCard.gearbox}</MDBListGroupItem>
-            <MDBListGroupItem>Miltal: {props.carCard.mileage}</MDBListGroupItem>
-            <MDBListGroupItem>Plats: {props.carCard.location}</MDBListGroupItem>
-          <MDBBtn href="#">MDBBtn</MDBBtn>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
-  )
+class CarCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+     };
+  }
+  render() {
+    return (
+      <MDBCol>
+        <MDBCard style={{ width: "22rem" }}>
+          {/* <MDBCardImage className="img-fluid" src={this.car.carImage} waves /> */}
+          <MDBCardBody>
+            <MDBCardTitle>{props.carCard.adTitle}</MDBCardTitle>
+            <MDBCardText>
+              {props.carCard.adDescription}
+            </MDBCardText>
+              <MDBListGroupItem>Pris: {props.carCard.price} SEK</MDBListGroupItem>
+              <MDBListGroupItem>Typ av bil: {props.carCard.carCategory}</MDBListGroupItem>
+              <MDBListGroupItem>Märke: {props.carCard.carBrand}</MDBListGroupItem>
+              <MDBListGroupItem>Modellår: {props.carCard.carModelYear}</MDBListGroupItem>
+              <MDBListGroupItem>Drivmedel: {props.carCard.carFuel}</MDBListGroupItem>
+              <MDBListGroupItem>Växellåda: {props.carCard.gearbox}</MDBListGroupItem>
+              <MDBListGroupItem>Miltal: {props.carCard.mileage}</MDBListGroupItem>
+              <MDBListGroupItem>Plats: {props.carCard.location}</MDBListGroupItem>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    );
+  }
 }
 
 export default CarCard;
