10681e3e835b1d0290420e644637732318392333
Amin <hamadagood111@gmail.com>
2019-11-14 00:28:18 +0100

    car's images is working now

--- frontend/src/components/CarForSale/CarForSaleCard/index.js
+++ frontend/src/components/CarForSale/CarForSaleCard/index.js
@@ -38,7 +38,8 @@ class CarForSaleCard extends React.Component {
     return (
       <MDBCol>
         <MDBCard style={{ width: "22rem" }}>
-          <MDBCardImage className="img-fluid" src={this.props.CarForSaleCard.carImage} waves />
+        <MDBCardImage className="img-fluid"
+           src={`http://localhost:5000/uploads/${this.props.CarForSaleCard.carImage}`} waves />
           <MDBCardBody>
             <MDBCardTitle>{this.props.CarForSaleCard.adTitle}</MDBCardTitle>
             <MDBCardText>
