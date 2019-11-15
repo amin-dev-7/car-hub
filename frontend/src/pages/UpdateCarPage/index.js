b7b89af4c9f983c7e34c3ed846c2c8fbdafeeae0
Amin <hamadagood111@gmail.com>
2019-11-16 00:52:28 +0100

    Working on update car

---
+++ frontend/src/pages/UpdateCarPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DefaultLayout from '../../layouts/index';
+import UpdateCar from '../../components/UpdateCar';
+
+const UpdateCarPage = () => {
+
+  return(
+  <DefaultLayout>
+
+    <UpdateCar />
+  </DefaultLayout >
+  );
+}
+
+export default UpdateCarPage;
