b9cb999f48d49f2a6fccaa4c18867451ac73ab73
Amin <hamadagood111@gmail.com>
2019-11-17 00:00:09 +0100

    Sort as category works

--- frontend/src/components/CarForSale/index.js
+++ frontend/src/components/CarForSale/index.js
@@ -4,25 +4,27 @@ import CarForSaleCard from './CarForSaleCard';
 import API from '../../cars-api';
 import {MDBBtn, MDBIcon} from 'mdbreact';
 import {Form } from 'react-bootstrap';
-import {MDBContainer, MBD} from 'mdbreact';
+import {MDBContainer} from 'mdbreact';
 class CarForSale extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       cars: [],
+      querCars: [],
+      isQuery: false,
       shwoFilter: false,
       carFilter: '',
-      query: ''
+      query: '',
      };
      this.toggelFilter = this.toggelFilter.bind(this);
      this.handleChange = this.handleChange.bind(this);
-    //  this.onSubmit = this.onSubmit.bind(this);
+     this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount = () => {
     this.getAllCars();
-    // this.onSubmit();
+    this.handleClick()
   }
 
   handleChange = e => {
@@ -37,18 +39,23 @@ class CarForSale extends React.Component {
     })
   }
 
-  // onSubmit =  () => {
-  //   API.get(`?carCategory=${this.state.carFilter}`)
-  //   .then(res => {
-  //     console.log(res.data)
-  //     this.setState({
-  //       cars: res.data
-  //     })
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
-  // }
+  handleClick = () => {
+    this.getQueryCars();
+  }
+
+  getQueryCars =  () => {
+    API.get(`?carCategory=${this.state.carFilter}`)
+    .then(res => {
+      console.log(res.data)
+        this.setState({
+          querCars: res.data,
+          isQuery: true
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
 
   getAllCars = () => {
     API.get()
@@ -62,46 +69,48 @@ class CarForSale extends React.Component {
     });
   }
 
-
   render() {
     let cars = this.state.cars;
+    let queryCars = this.state.querCars;
+    let card;
 
-    console.log(this.state.query);
-    console.log(this.state.carFilter)
+    if(queryCars.length > 0) {
+      card = queryCars.map(item =>
+        <CarForSaleCard key={item._id}
+          CarForSaleCard={item} >)
+        </CarForSaleCard>)
+    } else {
+      card = cars.map(item =>
+        <CarForSaleCard key={item._id}
+          CarForSaleCard={item} >)
+        </CarForSaleCard>)
+    }
 
-    let carOptionList = carOptions.map((option) =>
+    const carOptionList = carOptions.map((option) =>
     <option key={option.name}>{option.name}</option>);
 
     return (
       <div>
-      <MDBBtn color="primary" onClick={this.toggelFilter}>
-      <MDBIcon icon="filter" /> filtrera bilar </MDBBtn>
-
-      {this.state.shwoFilter &&
-        <MDBContainer>
-        <form onSubmit={this.onSubmit}>
-          <Form.Group controlId="exampleForm.ControlSelect2">
-            <br />
-              <p className="text">Typ av bil</p>
-              <Form.Control as="select" placeholder="Välj bilmärke"
-                onChange={this.handleChange}
-                value={this.state.carFilter}
-                name="carFilter">
-                <option value="" disabled>Välj ett alternativ...</option>
-                {carOptionList}
-              </Form.Control>
-            </Form.Group>
-            <MDBBtn color="btn btn-success" type="submit" className="font-weight-bold">
-              spara
-            </MDBBtn>
-          </form>
-        </MDBContainer>
-      }
-
-      {cars.map(item =>
-      <CarForSaleCard key={item._id}
-        CarForSaleCard={item} >)
-      </CarForSaleCard>)}
+        <MDBBtn color="primary" onClick={this.toggelFilter}>
+        <MDBIcon icon="filter" /> filtrera bilar </MDBBtn>
+        {this.state.shwoFilter &&
+          <MDBContainer>
+            <Form.Group controlId="exampleForm.ControlSelect2">
+              <br />
+                <Form.Control as="select" placeholder="Välj bilmärke"
+                  onChange={this.handleChange}
+                  value={this.state.carFilter}
+                  name="carFilter">
+                  <option value="" disabled>Välj ett alternativ...</option>
+                  {carOptionList}
+                </Form.Control>
+              </Form.Group>
+              <MDBBtn onClick={this.handleClick} color="btn btn-success" type="submit" className="font-weight-bold">
+                spara
+              </MDBBtn>
+          </MDBContainer>
+        }
+        {card}
       </div>
     );
   }
