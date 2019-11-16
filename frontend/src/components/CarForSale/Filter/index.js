88531e6a6bf98c97206df4fe33f893d7a3f8ffc4
Amin <hamadagood111@gmail.com>
2019-11-16 19:47:10 +0100

    Working on filter component's logic

--- frontend/src/components/CarForSale/Filter/index.js
+++ frontend/src/components/CarForSale/Filter/index.js
@@ -1,41 +1,53 @@
-import React from 'react'
-import carOptions from '../../../assets/data/car-options';
-import carModelYears from '../../../assets/data/car-model-year.json';
-import fuelOptions from '../../../assets/data/fuel-options.json';
-import carList from '../../../assets/data/car-list.json';
-import cities from '../../../assets/data/cities.json';
-import gearboxOptions from '../../../assets/data/gearbox-options.json';
-import {Form} from 'react-bootstrap';
-import {MDBContainer} from 'mdbreact';
+// import React from 'react'
+// import carOptions from '../../../assets/data/car-options';
+// import carModelYears from '../../../assets/data/car-model-year.json';
+// import fuelOptions from '../../../assets/data/fuel-options.json';
+// import carList from '../../../assets/data/car-list.json';
+// import cities from '../../../assets/data/cities.json';
+// import gearboxOptions from '../../../assets/data/gearbox-options.json';
+// import {Form} from 'react-bootstrap';
+// import {MDBContainer} from 'mdbreact';
 
-class Filter extends React.Component {
+// class Filter extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {  };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       carCategory: '',
+//      };
+//     this.handleChange = this.handleChange.bind(this)
+//   }
 
-  render() {
-    let carOptionList = carOptions.map((option) =>
-    <option key={option.name}>{option.name}</option>
-    );
+//   componentDidMount = () =>{
 
-    return (
-      <MDBContainer>
-      <Form.Group controlId="exampleForm.ControlSelect2">
-        <br />
-          <p className="text">Typ av bil</p>
-          <Form.Control as="select" placeholder="Välj bilmärke"
-            // onChange=""
-            // value=""
-            name="carCategory">
-            <option value="" disabled>Välj ett alternativ...</option>
-            {carOptionList}
-          </Form.Control>
-        </Form.Group>
-      </MDBContainer>
-    );
-  }
-}
+//   }
 
-export default Filter;
+//   handleChange = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//       });
+//   };
+
+//   render() {
+//     let carOptionList = carOptions.map((option) =>
+//     <option key={option.name}>{option.name}</option>
+//     );
+//     return (
+//       <MDBContainer>
+//       <Form.Group controlId="exampleForm.ControlSelect2">
+//         <br />
+//           <p className="text">Typ av bil</p>
+//           <Form.Control as="select" placeholder="Välj bilmärke"
+//             onChange={this.handleChange}
+//             value={this.props.carCategory}
+//             name="carCategory">
+//             <option value="" disabled>Välj ett alternativ...</option>
+//             {carOptionList}
+//           </Form.Control>
+//         </Form.Group>
+//       </MDBContainer>
+//     );
+//   }
+// }
+
+// export default Filter;
--- frontend/src/components/CarForSale/index.js
+++ frontend/src/components/CarForSale/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import carOptions from '../../assets/data/car-options';
 import CarForSaleCard from './CarForSaleCard';
 import API from '../../cars-api';
-import Fiter from './Filter'
 import {MDBBtn, MDBIcon} from 'mdbreact';
+import {Form } from 'react-bootstrap';
+import {MDBContainer, MBD} from 'mdbreact';
 class CarForSale extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       cars: [],
-      shwoFilter: false
+      shwoFilter: false,
+      carFilter: '',
+      query: ''
      };
-     this.state.toggelFilter = this.toggelFilter.bind(this);
+     this.toggelFilter = this.toggelFilter.bind(this);
+     this.handleChange = this.handleChange.bind(this);
+    //  this.onSubmit = this.onSubmit.bind(this);
   }
+
+  componentDidMount = () => {
+    this.getAllCars();
+    // this.onSubmit();
+  }
+
+  handleChange = e => {
+  this.setState({
+    [e.target.name]: e.target.value
+    });
+  };
 
   toggelFilter = () => {
     this.setState({
@@ -20,30 +37,67 @@ class CarForSale extends React.Component {
     })
   }
 
-  componentDidMount = () => {
-    this.getAllCars();
-  }
+  // onSubmit =  () => {
+  //   API.get(`?carCategory=${this.state.carFilter}`)
+  //   .then(res => {
+  //     console.log(res.data)
+  //     this.setState({
+  //       cars: res.data
+  //     })
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+  // }
 
   getAllCars = () => {
     API.get()
-      .then(res => {
-        this.setState({
-          cars: res.data
-        })
+    .then(res => {
+      this.setState({
+        cars: res.data
       })
-      .catch(error => {
-        console.log(error);
-      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
+
 
   render() {
     let cars = this.state.cars;
+
+    console.log(this.state.query);
+    console.log(this.state.carFilter)
+
+    let carOptionList = carOptions.map((option) =>
+    <option key={option.name}>{option.name}</option>);
 
     return (
       <div>
       <MDBBtn color="primary" onClick={this.toggelFilter}>
       <MDBIcon icon="filter" /> filtrera bilar </MDBBtn>
-      {this.state.shwoFilter && <Fiter/>}
+
+      {this.state.shwoFilter &&
+        <MDBContainer>
+        <form onSubmit={this.onSubmit}>
+          <Form.Group controlId="exampleForm.ControlSelect2">
+            <br />
+              <p className="text">Typ av bil</p>
+              <Form.Control as="select" placeholder="Välj bilmärke"
+                onChange={this.handleChange}
+                value={this.state.carFilter}
+                name="carFilter">
+                <option value="" disabled>Välj ett alternativ...</option>
+                {carOptionList}
+              </Form.Control>
+            </Form.Group>
+            <MDBBtn color="btn btn-success" type="submit" className="font-weight-bold">
+              spara
+            </MDBBtn>
+          </form>
+        </MDBContainer>
+      }
+
       {cars.map(item =>
       <CarForSaleCard key={item._id}
         CarForSaleCard={item} >)
