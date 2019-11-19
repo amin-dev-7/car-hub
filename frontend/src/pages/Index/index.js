56dc880dc35951f2f16f522356c8fe9700e3c18a
Amin <hamadagood111@gmail.com>
2019-11-19 02:43:15 +0100

    Delete unnecessary function

--- frontend/src/pages/Index/index.js
+++ frontend/src/pages/Index/index.js
@@ -17,15 +17,10 @@ class index extends React.Component {
      };
     this.getUser = this.getUser.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
-    this.updateUser = this.updateUser.bind(this);
   }
 
   componentDidMount = () => {
     this.getUser();
-  }
-
-  updateUser = (userObject) => {
-    this.setState(userObject)
   }
 
   getUser = () => {
